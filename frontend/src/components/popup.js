4439dec206212eb1307cd13c31552f83c6985e16
Mohit Sharma <mohit.vsht@gmail.com>
2023-05-24 20:16:54 +0530

    restoring old fn

--- frontend/src/components/popup.js
+++ frontend/src/components/popup.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState } from 'react';
 import '../static/css/popup.css';
 import galaxy from '../static/img/galaxy.png'
@@ -6,7 +5,9 @@ import galaxy from '../static/img/galaxy.png'
 const Popup = () => {
 
     const [metadata, setMetadata] = useState({});
-    console.log(metadata.media);
+    const [tokenContract, setTokenContract] = useState('');
+    const [tokenId, setTokenId] = useState(undefined);
+    const [NFTValue, setNFTValue] = useState(undefined);
 
     function hidePopup() {
         try {
@@ -17,22 +18,36 @@ const Popup = () => {
         setMetadata({});
     }
 
-    function fetchNFT() {
-        setMetadata({
-            contract: {
-                address: "0xcbf0232a0b8cb5f0b41a0a9736332223fab338ca",
-                contractDeployer: undefined,
-                deployedBlockNumber: undefined,
-                name: "Crypto Devs"
-            },
-            description: "Crypto Dev is a collection of developers in crypto",
-            media: [
-                { gateway: 'https://raw.githubusercontent.com/LearnWeb3DAO/NFT-Collection/main/my-app/public/cryptodevs/11.svg', raw: 'https://raw.githubusercontent.com/LearnWeb3DAO/NFT-Collection/main/my-app/public/cryptodevs/11.svg' }]
-        })
-    }
+    // function fetchNFT() {
+    //     setMetadata({
+    //         contract: {
+    //             address: "0xcbf0232a0b8cb5f0b41a0a9736332223fab338ca",
+    //             contractDeployer: undefined,
+    //             deployedBlockNumber: undefined,
+    //             name: "Crypto Devs"
+    //         },
+    //         description: "Crypto Dev is a collection of developers in crypto",
+    //         media: [
+    //             { gateway: 'https://raw.githubusercontent.com/LearnWeb3DAO/NFT-Collection/main/my-app/public/cryptodevs/11.svg', raw: 'https://raw.githubusercontent.com/LearnWeb3DAO/NFT-Collection/main/my-app/public/cryptodevs/11.svg' }]
+    //     })
+    // }
 
     function getNFTColletralValue() {
         return 8000;
+    }
+
+    const handleClick = async () => {
+      if(tokenContract && tokenId !==null){
+      const data = await getmetadata(tokenContract, tokenId);
+      setMetadata(data);
+    }
+    else {
+      alert("Enter NFT contract Address and Id")
+    }
+      const nftvalue = await getNftCollateralValue(Contract, '0xcBF0232a0b8Cb5f0b41a0a9736332223faB338cA', '0xcBF0232a0b8Cb5f0b41a0a9736332223faB338cA');
+  
+      setNFTValue(nftvalue);
+      console.log(nftvalue);
     }
 
     return (
@@ -43,7 +58,7 @@ const Popup = () => {
                 <div className="right_popup">
                     <input type="text" placeholder='Enter token contract' />
                     <input type="text" placeholder='Enter token ID' />
-                    <button onClick={() => fetchNFT()}>Fetch NFT</button>
+                    <button onClick={() => handleClick()}>Fetch NFT</button>
                     <br />
                     {(metadata.contract == undefined ?
                         <div className="lend_absent">
@@ -79,70 +94,4 @@ const Popup = () => {
 }
 
 export default Popup;
-=======
-import React, { useState } from 'react';
-import '../static/css/popup.css';
-import { getmetadata, getNftCollateralValue } from '../backend'
 
-const Popup = ({Contract}) => {
-  const [tokenContract, setTokenContract] = useState('');
-  const [tokenId, setTokenId] = useState(undefined);
-  const [NFTValue, setNFTValue] = useState(undefined);
-  const [Metadata, setMetadata] = useState(null);
-
-  function hidePopup() {
-    try {
-      let k = document.getElementsByClassName('lend_popup');
-      console.log(k);
-      k[0].style.display = 'none';
-    } catch {
-
-    }
-  }
-
-  const handleClick = async () => {
-    if(tokenContract && tokenId !==null){
-    const data = await getmetadata(tokenContract, tokenId);
-    setMetadata(data);
-  }
-  else {
-    alert("Enter NFT contract Address and Id")
-  }
-    const nftvalue = await getNftCollateralValue(Contract, '0xcBF0232a0b8Cb5f0b41a0a9736332223faB338cA', '0xcBF0232a0b8Cb5f0b41a0a9736332223faB338cA');
-
-    setNFTValue(nftvalue);
-    console.log(nftvalue);
-  }
-
-  return (
-    <div>
-      <div className="lend_popup">
-        <div className="left_popup">
-          {Metadata && tokenContract && tokenId!== null 
-            ? <img src={`${Metadata.rawMetadata.image}`} alt="NFT"/> 
-            : null
-          }
-        </div>
-        <div className="right_popup">
-          <input type="text" placeholder='Enter token contract' onChange={(e) => setTokenContract(e.target.value)} />
-          <input type="number" placeholder='Enter token ID' onChange={(e) => setTokenId(e.target.value)} />
-          <button onClick={() => {handleClick()}}>Fetch NFT</button>
-          <br />
-          <input type="text" value={Metadata !== null ? 'Name: ' + Metadata.rawMetadata.name : 'Name: '}/>
-          <input type="text" value={Metadata !== null ? 'Description: ' + Metadata.rawMetadata.description : 'Description: '}/>
-          <input type="text" value={NFTValue !== undefined ? 'NFTs Value: ' + NFTValue : 'NFTs Value: '}/>
-          <input type="text" value={NFTValue !== undefined ? 'NFTs Value: ' + NFTValue : 'NFTs Value: '}/>
-          <input type="text" />
-          <button>Borrow</button>
-        </div>
-        <i className='bi bi-x' onClick={hidePopup}></i>
-      </div>
-    </div>
-  );
-}
-
-export default Popup;
-
-// 0xcBF0232a0b8Cb5f0b41a0a9736332223faB338cA
-// tokenId = 11
->>>>>>> 36b5bed7cf212ac4b4e11c176591ff947eb060c7
